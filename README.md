8c44418695ac3f930e5ebe8f2571f962e7bee7de
laihanbin <512412344@qq.com>
2022-10-27 11:52:03 +0800

    build: set base pathname

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # g6-demo
 
 @antv/g6 练习
+
+[预览地址](https://humandetail.github.io/g6-demo)
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, loadEnv } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    base: env.VITE_APP_PATHNAME,
+  }
+})
